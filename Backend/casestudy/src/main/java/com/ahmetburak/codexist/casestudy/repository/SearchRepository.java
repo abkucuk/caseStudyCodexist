249f5c046a64efc7fa2ed47e4b484de255731b17
abkucuk <ahmetbkucuk@gmail.com>
2022-10-04 10:51:18 +0300

    add JPQuery for existing search model.

--- Backend/casestudy/src/main/java/com/ahmetburak/codexist/casestudy/repository/SearchRepository.java
+++ Backend/casestudy/src/main/java/com/ahmetburak/codexist/casestudy/repository/SearchRepository.java
@@ -1,9 +1,12 @@
 package com.ahmetburak.codexist.casestudy.repository;
 import com.ahmetburak.codexist.casestudy.model.Search;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SearchRepository extends JpaRepository<Search, Long> {
-    Optional<Search> findByLatitudeAndLongitudeAndRadius (String latitude, String longitude, String radius);
+    @Query("select s from Search s where s.latitude = :latitude and s.longitude = :longitude and s.radius = :radius  ")
+    Optional<Search> findByLatitudeAndLongitudeAndRadius ( @Param("latitude") String latitude, @Param("longitude") String longitude,  @Param("radius") String radius);
 }
